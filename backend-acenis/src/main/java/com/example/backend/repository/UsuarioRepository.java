13de0dccd1a7777b0a4c8f9c21edb6fcf5b28357
Gustavo <gustavogarbim850@gmail.com>
2025-06-06 19:58:53 -0300

    (fix) Pequenas correcoes

--- backend-acenis/src/main/java/com/example/backend/repository/UsuarioRepository.java
+++ backend-acenis/src/main/java/com/example/backend/repository/UsuarioRepository.java
@@ -1,10 +1,10 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.TipoUsuario; // Importe o enum
+import com.example.backend.model.TipoUsuario;
 import com.example.backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List; // Importe o List
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
