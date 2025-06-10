f2d47301f27d518d66ecc8376d1f30b8f67f86c4
Gustavo <gustavogarbim850@gmail.com>
2025-06-09 21:37:00 -0300

    desativada a barreira Cross

--- backend-acenis/src/main/java/com/example/backend/controller/UsuarioController.java
+++ backend-acenis/src/main/java/com/example/backend/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:3000", "login-acenis.vercel.app/"})
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
