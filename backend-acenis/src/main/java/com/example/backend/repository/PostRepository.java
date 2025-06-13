e4309f15f953d256d1da48e8c74c11d8456fba07
CarlosEduardoVitor <165283191+CarlosEduardoVitor@users.noreply.github.com>
2025-06-13 11:00:45 -0300

    Update post2

--- backend-acenis/src/main/java/com/example/backend/repository/PostRepository.java
+++ backend-acenis/src/main/java/com/example/backend/repository/PostRepository.java
@@ -4,6 +4,6 @@ import com.example.backend.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Interger> {
-    List<Post> findByAutorId(Interget idUser);
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByAutorId(Integet idUser);
 }
