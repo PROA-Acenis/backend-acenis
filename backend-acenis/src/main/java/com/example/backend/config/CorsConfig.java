44185021c13aa1e5c19babe180b23b497c248ccf
CarlosEduardoVitor <165283191+CarlosEduardoVitor@users.noreply.github.com>
2025-06-15 19:40:38 -0300

    UpdateConfig Cors

--- backend-acenis/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend-acenis/src/main/java/com/example/backend/config/CorsConfig.java
@@ -14,11 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://backend-acenis-production.up.railway.app"
-                                // "https://SEU-DOMINO-FRONTEND-PRODUCAO.com" //
-                        )
+                        .allowedOrigin("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
